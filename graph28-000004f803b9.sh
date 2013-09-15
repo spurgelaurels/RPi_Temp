c4ea12b2e8958d927dce2b0e4f1bca7c34766a10
dreed <dave@davereed.ca>
2013-09-15 14:44:38 -0400

    update

--- graph28-000004f803b9.sh
+++ graph28-000004f803b9.sh
@@ -11,26 +11,26 @@ CDEF:trend=temp,1800,TREND \
 LINE2:temp$RAWCOLOUR:"Hourly temperature" \
 LINE1:trend$TRENDCOLOUR:"30 min average"
 
+#6h
+rrdtool graph $RRDPATH/master-day.png --start -6h \
+DEF:temp=$RRDPATH/28-000004f75d99.rrd:temp:AVERAGE \
+CDEF:trend=temp,5400,TREND \
+LINE2:temp$RAWCOLOUR:"6hr temperature" \
+LINE1:trend$TRENDCOLOUR:"1.5hr average"
+
 #day
-rrdtool graph $RRDPATH/master-day.png --start -1d \
+rrdtool graph $RRDPATH/master-week.png --start -1d \
 DEF:temp=$RRDPATH/28-000004f75d99.rrd:temp:AVERAGE \
-CDEF:trend=temp,21600,TREND \
 LINE2:temp$RAWCOLOUR:"Daily temperature" \
-LINE1:trend$TRENDCOLOUR:"6h average"
-#
+
 #week
-rrdtool graph $RRDPATH/master-week.png --start -1w \
+rrdtool graph $RRDPATH/master-month.png --start -1w \
 DEF:temp=$RRDPATH/28-000004f75d99.rrd:temp:AVERAGE \
-LINE2:temp$RAWCOLOUR:"Weekly temperature" \
+LINE1:temp$RAWCOLOUR:"Weekly temperature" \
 
-#month
-rrdtool graph $RRDPATH/master-month.png --start -1m \
+#3mos
+rrdtool graph $RRDPATH/master-year.png --start -3m \
 DEF:temp=$RRDPATH/28-000004f75d99.rrd:temp:AVERAGE \
-LINE1:temp$RAWCOLOUR:"Monthly temperature" \
-
-#year
-rrdtool graph $RRDPATH/master-year.png --start -1y \
-DEF:temp=$RRDPATH/28-000004f75d99.rrd:temp:AVERAGE \
-LINE1:temp$RAWCOLOUR:"Yearly temperature" \
+LINE1:temp$RAWCOLOUR:"3mos temperature" \
 #
 
